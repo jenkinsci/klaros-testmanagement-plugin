8d0453bfaf1d754811ba721b66547ce5696102c3
stolp <stolp@dev.java.net>
2010-03-19 15:29:55 +0000

    Support for user authentication.

--- src/main/java/hudson/plugins/klaros/KlarosTestResultPublisher.java
+++ src/main/java/hudson/plugins/klaros/KlarosTestResultPublisher.java
@@ -52,10 +52,10 @@ import javax.servlet.http.HttpServletResponse;
 import net.sf.json.JSONObject;
 
 import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.methods.FileRequestEntity;
 import org.apache.commons.httpclient.methods.PutMethod;
 import org.apache.commons.httpclient.methods.RequestEntity;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.apache.tools.ant.DirectoryScanner;
 import org.apache.tools.ant.types.FileSet;
 import org.kohsuke.stapler.AncestorInPath;
@@ -73,12 +73,29 @@ import org.kohsuke.stapler.StaplerRequest;
  */
 public class KlarosTestResultPublisher extends Recorder {
 
+    /** The config. */
     private String config;
+
+    /** The env. */
     private String env;
+
+    /** The sut. */
     private String sut;
+
+    /** The type. */
     private String type = "junit";
+
+    /** The path test results. */
     private String pathTestResults;
+
+    /** The url. */
     private String url;
+
+    /** The username. */
+    private String username;
+
+    /** The password. */
+    private String password;
 
     /**
      * Instantiates a new klaros test result publisher.
@@ -95,17 +112,23 @@ public class KlarosTestResultPublisher extends Recorder {
      *            the path to the test results
      * @param url
      *            the Klaros application url
+     * @param username
+     *            the optional Klaros login user name
+     * @param password
+     *            the optional Klaros login password
      */
     @DataBoundConstructor
     public KlarosTestResultPublisher(final String config, final String env,
             final String sut, final String type, final String pathTestResults,
-            final String url) {
+            final String url, final String username, final String password) {
 
         this.config = config;
         this.env = env;
         this.sut = sut;
         this.pathTestResults = pathTestResults;
         this.url = url;
+        this.username = username;
+        this.password = password;
     }
 
     /**
@@ -118,54 +141,151 @@ public class KlarosTestResultPublisher extends Recorder {
                 KlarosTestResultPublisher.DescriptorImpl.class);
     }
 
+    /**
+     * Gets the config.
+     *
+     * @return the config
+     */
     public String getConfig() {
 
         return config;
     }
 
-    public void setConfig(final String config) {
+    /**
+     * Sets the config.
+     *
+     * @param value
+     *            the new config
+     */
+    public void setConfig(final String value) {
 
-        this.config = config;
+        config = value;
     }
 
+    /**
+     * Gets the env.
+     *
+     * @return the env
+     */
     public String getEnv() {
 
         return env;
     }
 
-    public void setEnv(final String env) {
+    /**
+     * Sets the env.
+     *
+     * @param value
+     *            the new env
+     */
+    public void setEnv(final String value) {
 
-        this.env = env;
+        env = value;
     }
 
+    /**
+     * Gets the url.
+     *
+     * @return the url
+     */
     public String getUrl() {
 
         return url;
     }
 
-    public void setUrl(final String url) {
+    /**
+     * Sets the url.
+     *
+     * @param value
+     *            the new url
+     */
+    public void setUrl(final String value) {
 
-        this.url = url;
+        url = value;
     }
 
+    /**
+     * Gets the username.
+     *
+     * @return the username
+     */
+    public String getUsername() {
+
+        return username;
+    }
+
+    /**
+     * Sets the username.
+     *
+     * @param value
+     *            the new username
+     */
+    public void setUsername(final String value) {
+
+        username = value;
+    }
+
+    /**
+     * Gets the password.
+     *
+     * @return the password
+     */
+    public String getPassword() {
+
+        return password;
+    }
+
+    /**
+     * Sets the password.
+     *
+     * @param value
+     *            the new password
+     */
+    public void setPassword(final String value) {
+
+        password = value;
+    }
+
+    /**
+     * Gets the sut.
+     *
+     * @return the sut
+     */
     public String getSut() {
 
         return sut;
     }
 
-    public void setSut(final String sut) {
+    /**
+     * Sets the sut.
+     *
+     * @param value
+     *            the new sut
+     */
+    public void setSut(final String value) {
 
-        this.sut = sut;
+        sut = value;
     }
 
+    /**
+     * Gets the path test results.
+     *
+     * @return the path test results
+     */
     public String getPathTestResults() {
 
         return pathTestResults;
     }
 
-    public void setPathTestResults(final String pathTestResults) {
+    /**
+     * Sets the path test results.
+     *
+     * @param value
+     *            the new path test results
+     */
+    public void setPathTestResults(final String value) {
 
-        this.pathTestResults = pathTestResults;
+        pathTestResults = value;
     }
 
     /**
@@ -247,6 +367,7 @@ public class KlarosTestResultPublisher extends Recorder {
      *
      * @param sourceURL
      *            the URL of the klaros server import servlet
+     *
      * @return the klaros server import servlet URL
      */
     public String getUrl(final String sourceURL) {
@@ -289,6 +410,28 @@ public class KlarosTestResultPublisher extends Recorder {
     }
 
     /**
+     * Builds the servlet url. Try to honor URL's with trailing slashes.
+     *
+     * @param applicationURL
+     *            the application url
+     *
+     * @return the servlet url
+     */
+    private static String buildServletURL(final String applicationURL) {
+
+        final String result;
+        if (applicationURL.endsWith("/")) {
+            result = new StringBuffer(applicationURL.substring(0,
+                    applicationURL.length() - 1)).append(
+                    "/seam/resource/rest/importer").toString();
+        } else {
+            result = new StringBuffer(applicationURL).append(
+                    "/seam/resource/rest/importer").toString();
+        }
+        return result;
+    }
+
+    /**
      * The Class FileCallableImplementation.
      */
     private final class FileCallableImplementation implements
@@ -297,8 +440,15 @@ public class KlarosTestResultPublisher extends Recorder {
         /** The serial version UID. */
         private static final long serialVersionUID = 1560913900801548965L;
 
+        /** The listener. */
         private final BuildListener listener;
 
+        /**
+         * Instantiates a new file callable implementation.
+         *
+         * @param listener
+         *            the listener
+         */
         private FileCallableImplementation(final BuildListener listener) {
             this.listener = listener;
         }
@@ -335,43 +485,67 @@ public class KlarosTestResultPublisher extends Recorder {
             // Get target URL
             String targetUrl = getUrl(url);
             if (targetUrl != null) {
-                String strURL = new StringBuffer(targetUrl).append(
-                        "/seam/resource/rest/importer").toString();
+                String strURL = buildServletURL(targetUrl);
 
                 // Prepare HTTP PUT
                 for (String f : ds.getIncludedFiles()) {
                     PutMethod put = new PutMethod(strURL);
-                    String query = "config=" + config + "&env=" + env + "&sut="
-                            + sut + "&type=" + type;
-                    put.setQueryString(query);
+                    StringBuffer query = new StringBuffer("config=").append(
+                            config).append("&env=").append(env).append("&sut=")
+                            .append(sut).append("&type=").append(type);
+                    if (username != null) {
+                        query.append("&username=").append(username).append(
+                                "&password=").append(password);
+                    }
+                    put.setQueryString(query.toString());
 
                     File file = new File(baseDir, f);
                     int result;
                     try {
-                        result = executePut(file, put);
-                        if (result != HttpServletResponse.SC_OK) {
-                            StringBuffer msg = new StringBuffer().append(
-                                    "Export of ").append(file.getName())
-                                    .append(" failed - Response status code: ")
-                                    .append(result)
-                                    .append(" for request URL: ")
-                                    .append(strURL).append("?").append(query);
-                            String response = put.getResponseBodyAsString();
-                            if (response != null && response.length() > 0) {
-                                msg.append("\nReason: ").append(response);
+                        RequestEntity entity = new FileRequestEntity(file,
+                                "text/xml; charset=ISO-8859-1");
+                        put.setRequestEntity(entity);
+                        // Get HTTP client
+                        HttpClient httpclient = new HttpClient();
+
+                        // Execute request
+                        try {
+                            result = httpclient.executeMethod(put);
+
+                            if (result != HttpServletResponse.SC_OK) {
+                                StringBuffer msg = new StringBuffer()
+                                        .append("Export of ")
+                                        .append(file.getName())
+                                        .append(
+                                                " failed - Response status code: ")
+                                        .append(result).append(
+                                                " for request URL: ").append(
+                                                strURL).append("?").append(
+                                                query);
+                                String response = new String(put
+                                        .getResponseBody());
+                                if (response != null && response.length() > 0) {
+                                    msg.append("\nReason: ").append(response);
+                                }
+                                listener.getLogger().println(msg.toString());
+                            } else {
+                                results.add(result);
+                                listener
+                                        .getLogger()
+                                        .println(
+                                                "Test result file "
+                                                        + file.getName()
+                                                        + " has been successfully exported.");
                             }
-                            listener.getLogger().println(msg.toString());
-                        } else {
-                            results.add(result);
-                            listener
-                                    .getLogger()
-                                    .println(
-                                            "Test result file "
-                                                    + file.getName()
-                                                    + " has been successfully exported.");
+                        } catch (Exception e) {
+                            e.printStackTrace(listener.getLogger());
                         }
-                    } catch (Exception e) {
-                        e.printStackTrace(listener.getLogger());
+                    } finally {
+                        // Release current connection to the connection pool
+                        // once you
+                        // are
+                        // done
+                        put.releaseConnection();
                     }
                 }
             } else {
@@ -380,48 +554,6 @@ public class KlarosTestResultPublisher extends Recorder {
             }
             return results;
         }
-
-        /**
-         * Execute the method post.
-         *
-         * @param file
-         *            the file to put
-         * @param put
-         *            the put method to execute
-         *
-         * @throws IOException
-         *             Signals that an I/O exception of some sort has occurred.
-         * @throws HttpException
-         *             Signals that an HTTP exception of some sort has occurred.
-         *
-         * @return The return code of the put method execution.
-         */
-        private int executePut(final File file, final PutMethod put)
-                throws HttpException, IOException {
-
-            // Request content will be retrieved directly
-            // from the input stream
-            int result = 0;
-            RequestEntity entity = new FileRequestEntity(file,
-                    "text/xml; charset=ISO-8859-1");
-            put.setRequestEntity(entity);
-            // Get HTTP client
-            HttpClient httpclient = new HttpClient();
-
-            // Execute request
-            try {
-                result = httpclient.executeMethod(put);
-
-            } finally {
-                // Release current connection to the connection pool once you
-                // are
-                // done
-                put.releaseConnection();
-            }
-            return result;
-
-        }
-
     }
 
     /**
@@ -432,15 +564,20 @@ public class KlarosTestResultPublisher extends Recorder {
     public static final class DescriptorImpl extends
             BuildStepDescriptor<Publisher> {
 
+        /** The Constant PROJECT_CONFIG_HTML. */
         private static final String PROJECT_CONFIG_HTML = //
         "/plugin/klaros-testmanagement/help-projectConfig.html";
+
+        /** The Constant URL_NAME. */
         private static final String URL_NAME = "url.name";
-        /**
-         * Global configuration information.
-         */
+
+        /** Global configuration information. */
 
         private List<String> urls = new ArrayList<String>();
 
+        /**
+         * Instantiates a new descriptor impl.
+         */
         public DescriptorImpl() {
 
             load();
@@ -492,6 +629,11 @@ public class KlarosTestResultPublisher extends Recorder {
             return PROJECT_CONFIG_HTML;
         }
 
+        /**
+         * Gets the urls.
+         *
+         * @return the urls
+         */
         public List<String> getUrls() {
 
             return urls;
@@ -516,15 +658,16 @@ public class KlarosTestResultPublisher extends Recorder {
          *
          * @param value
          *            the url value to check
+         *
+         * @return the form validation result
+         *
          * @throws IOException
          *             Signals that an I/O exception has occurred.
          * @throws ServletException
          *             the servlet exception
-         *
-         * @return the form validation result
          */
-        public FormValidation doCheckUrl(final String value) throws IOException,
-                ServletException {
+        public FormValidation doCheckUrl(final String value)
+                throws IOException, ServletException {
 
             return new FormValidation.URLCheck() {
 
@@ -602,10 +745,77 @@ public class KlarosTestResultPublisher extends Recorder {
             if (Util.fixEmpty(value) != null) {
                 return FormValidation.ok();
             } else {
-                return FormValidation.error(Messages.ErrorMissingInstallation());
+                return FormValidation
+                        .error(Messages.ErrorMissingInstallation());
             }
         }
 
-    }
+        /**
+         * Test the connection with the given parameters.
+         *
+         * @param url
+         *            the url
+         * @param username
+         *            the username
+         * @param password
+         *            the password
+         *
+         * @return the form validation
+         *
+         * @throws IOException
+         *             Signals that an I/O exception has occurred.
+         * @throws ServletException
+         *             the servlet exception
+         */
+        public FormValidation doTestConnection(
+                @QueryParameter final String url,
+                @QueryParameter final String username,
+                @QueryParameter final String password) throws IOException,
+                ServletException {
 
+            final String strURL = buildServletURL(url);
+
+            PutMethod put = new PutMethod(strURL);
+            StringBuffer query = new StringBuffer();
+            if (username != null) {
+                query.append("username=").append(username).append("&password=")
+                        .append(password).append("&type=").append("check");
+            }
+            System.out.println(strURL + '?' + query.toString());
+            put.setQueryString(query.toString());
+            try {
+                RequestEntity entity = new StringRequestEntity("",
+                        "text/xml; charset=UTF-8", "UTF-8");
+                put.setRequestEntity(entity);
+                int result;
+                try {
+                    HttpClient client = new HttpClient();
+                    result = client.executeMethod(put);
+                    String response = "";
+                    if (result != HttpServletResponse.SC_OK) {
+                        StringBuffer msg = new StringBuffer();
+                        response = new String(put.getResponseBody());
+                        if (response != null && response.length() > 0) {
+                            msg.append("Connection failed: ").append(response);
+                            System.out.println(msg.toString());
+                        }
+                        return FormValidation.error(msg.toString());
+                    } else {
+                        if (response != null && response.length() > 0) {
+                            return FormValidation.ok(Messages
+                                    .ConnectionEstablished()
+                                    + ": " + response);
+                        } else {
+                            return FormValidation.ok(Messages
+                                    .ConnectionEstablished());
+                        }
+                    }
+                } finally {
+                    put.releaseConnection();
+                }
+            } catch (Exception e) {
+                return FormValidation.error(e.getMessage());
+            }
+        }
+    }
 }
